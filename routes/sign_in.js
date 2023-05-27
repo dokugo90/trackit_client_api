4bd077aacdb229f1e4f261eedd195893d0e0fcd3
dokugo90 <dokugo90@gmail.com>
2023-05-27 23:46:51 +0000

    Testing

--- routes/sign_in.js
+++ routes/sign_in.js
@@ -7,7 +7,24 @@ const User = require("../models/user");
 
 
 router.post("/sign_in",  async (req, res) => {
-    res.send("Hey sign_in")
+    try {
+        const user = await User.findOne({ email: req.body.email }).exec();
+    
+        if (!user) {
+          return res.send({ message: 'Account does not exist' });
+        }
+    
+        const isMatch = await bcrypt.compare(req.body.password, user.password);
+        if (!isMatch) {
+          return res.send({ message: 'Invalid password' });
+        }
+    
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+        res.send({ token });
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: 'Server error' });
+      }
 });
 
 module.exports = router;
